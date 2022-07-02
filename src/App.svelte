b048e5e77191c5e43be93c4d0df09d0fc4233342
Frenzod <frenzoid@protonmail.com>
2022-07-02 04:18:57 +0200

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "./css/tailwind.css";
+  import Web3 from "./components/web3.svelte";
+</script>
+
+<h1 class="text-3xl font-bold underline text-center mt-4 mb-2">Hello world!</h1>
+<Web3 />
